b3070148564938461ea04133cbf27a9c95c3fd13
fire8327 <fire83274@gmail.com>
2023-04-05 02:17:29 +0300

    create all

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,25 +1,7 @@
-/* navigation */
 /*jshint esnext: true */
-const toggler = document.getElementById("toggler");
-const mNav = document.getElementById("mobile__nav");
-const close = document.getElementById("toggler__expanded");
-const overlay = document.getElementById("overlay");
-const body = document.getElementsByTagName("body")[0];
+const html = document.querySelector("html");
+const toggler = document.querySelector("#toggler");
 
-toggler.addEventListener("click", () => {
-  mNav.classList.remove("translate-x-full");
-  overlay.classList.remove("hidden");
-  body.classList.add("overflow-hidden");
-});
-
-close.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden");
-  body.classList.remove("overflow-hidden"); 
-});
-
-overlay.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden"); 
-  body.classList.remove("overflow-hidden"); 
+toggler.addEventListener("click",() => {
+  html.classList.toggle("dark");
 });
